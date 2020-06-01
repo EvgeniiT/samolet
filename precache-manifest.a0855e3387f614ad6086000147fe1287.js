d95ca645e261f0caafddf33d3f401cca63cb5afb
Evgenii Tkachev <3363867@gmail.com>
2020-06-01 10:51:07 +0300

    Updates

---
+++ precache-manifest.a0855e3387f614ad6086000147fe1287.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225f596fac444c4337bfeebef541b9f0",
+    "url": "/samolet/index.html"
+  },
+  {
+    "revision": "443cb7a65462ad15f70b",
+    "url": "/samolet/static/css/2.81187608.chunk.css"
+  },
+  {
+    "revision": "c5b0951f6e1034b98c66",
+    "url": "/samolet/static/css/main.7db912ed.chunk.css"
+  },
+  {
+    "revision": "443cb7a65462ad15f70b",
+    "url": "/samolet/static/js/2.5d379253.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/samolet/static/js/2.5d379253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b0951f6e1034b98c66",
+    "url": "/samolet/static/js/main.a37b7406.chunk.js"
+  },
+  {
+    "revision": "867ab961fa7f00a7b24e",
+    "url": "/samolet/static/js/runtime-main.c57ca51e.js"
+  }
+]);
